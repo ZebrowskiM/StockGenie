0667bea47b769262dda33cc7b0cfa8b07b5d5e39
Marek Zebrowski <zebrowskimarek0@outlook.com>
2021-10-16 17:56:53 -0400

    Adding External assemblies for crypto and stocks

---
+++ StockGenie.DataObjects/Crypto.cs
@@ -0,0 +1,22 @@
+using System;
+/*
+Things to include:
+Current Price 
+Bid Ask 
+52 high
+52 low 
+Average 
+50 MA 
+9 or 13 day moving average 
+Market cap 
+Description and Link to site ( maybe )
+Coin description and maybe usecases  ( optional) 
+
+##Figure out what exchange to base prices on or include multiple##
+*/
+namespace StockGenie.DataObjects
+{
+    public class Crypto
+    {
+    }
+}
